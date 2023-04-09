1dc88ed60acd8c2778071436f1e3448b16ee36da
Iurii Sydorov <iurii.sydorov@gmail.com>
2023-04-09 10:59:28 +0300

    start

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,48 +1,35 @@
-import article from 'data/article';
-import data from 'data/data.json';
-
-import {
-  BlogCard,
-  Container,
-  Section,
-  Heading,
-  Statistics,
-  ForbesList,
-  CryptoHistory,
-} from 'components';
+import { Tab, TabList, TabPanel, Tabs } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+import { Container, Header, Section, Text } from 'components';
+import { Gallery, Todos } from 'tabs';
 
 export const App = () => {
   return (
-    <Section>
-      <Container>
-        <Heading marginBottom="50px" textAlign="center">
-          Task 1
-        </Heading>
-        <BlogCard
-          poster={article.poster}
-          tag={article.tag}
-          title={article.title}
-          description={article.description}
-          userName={article.name}
-          avatar={article.avatar}
-          postedAt={article.postedAt}
-        />
+    <>
+      <Header />
 
-        <Heading marginTop="50px" marginBottom="50px" textAlign="center">
-          Task 2
-        </Heading>
-        <Statistics title="Main Statistics" data={data} />
+      <Section>
+        <Container>
+          <Tabs>
+            <TabList>
+              <Tab>
+                <Text>Gallery</Text>
+              </Tab>
+              <Tab>
+                <Text>Todos</Text>
+              </Tab>
+            </TabList>
 
-        <Heading marginTop="50px" marginBottom="50px" textAlign="center">
-          Task 3
-        </Heading>
-        <ForbesList />
+            <TabPanel>
+              <Gallery />
+            </TabPanel>
 
-        <Heading marginTop="50px" marginBottom="50px" textAlign="center">
-          Task 4
-        </Heading>
-        <CryptoHistory />
-      </Container>
-    </Section>
+            <TabPanel>
+              <Todos />
+            </TabPanel>
+          </Tabs>
+        </Container>
+      </Section>
+    </>
   );
 };
